79639c87ff5715fa4a7c267a0b665a49f9e3d656
Gianni Chiappetta <gianni@runlevel6.org>
2010-08-30 09:56:56 -0400

    Initial commit.

---
+++ spec/levenshtein.spec.js
@@ -0,0 +1,22 @@
+// Tests
+var Levenshtein = require( '../lib/Levenshtein' )
+
+
+exports['test Levenshtein constructor'] = function( assert ) {
+  var l1 = new Levenshtein( 'kitten', 'sitting' )
+    , l2 = new Levenshtein( 'Saturday', 'Sunday' )
+
+  assert.equal( 3, l1.distance, 'kitten <=> sitting' )
+  assert.equal( 3, l2.distance, 'Saturday <=> Sunday' )
+}
+
+exports['test Levenshtein can be coerced to a number'] = function( assert ) {
+  var l1 = new Levenshtein( 'kitten', 'sitting' )
+  assert.equal( l1.distance, l1 + 0 )
+}
+
+exports['test Levenshtein can be coerced to a string'] = function( assert ) {
+  var l1 = new Levenshtein( 'kitten', 'sitting' )
+  assert.equal( l1.inspect(), new String(l1) )
+}
+
