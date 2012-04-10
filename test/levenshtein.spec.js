83e1173382332e3e3f70600fbc422383364d0339
Niklas Hambüchen <mail@nh2.me>
2012-04-10 05:15:23 +0100

    Fix tests, use expresso

--- test/levenshtein.spec.js
+++ test/levenshtein.spec.js
@@ -1,7 +1,9 @@
 // Tests
-var Levenshtein = require( '../lib/Levenshtein' )
+// Run with `expresso` from the toplevel directory.
+var Levenshtein = require( '../lib/levenshtein' )
+var assert = require( 'assert' )
 
-exports['test Levenshtein correctness'] = function( assert ) {
+exports['test Levenshtein correctness'] = function() {
   var l1 = new Levenshtein( 'kitten', 'sitting' )
     , l2 = new Levenshtein( 'Saturday', 'Sunday' )
 
@@ -9,12 +11,12 @@ exports['test Levenshtein correctness'] = function( assert ) {
   assert.equal( 3, l2.distance, 'Saturday <=> Sunday' )
 }
 
-exports['test Levenshtein can be coerced to a number'] = function( assert ) {
+exports['test Levenshtein can be coerced to a number'] = function() {
   var l1 = new Levenshtein( 'kitten', 'sitting' )
   assert.equal( l1.distance, l1 + 0 )
 }
 
-exports['test Levenshtein can be coerced to a string'] = function( assert ) {
+exports['test Levenshtein can be coerced to a string'] = function() {
   var l1 = new Levenshtein( 'kitten', 'sitting' )
   assert.equal( l1.inspect(), new String(l1) )
 }
